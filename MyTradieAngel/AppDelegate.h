2ec001d3acee1a945011b7efa6ee02b043e0f222
RenZhe Ahn <cuiguangmin@Cuis-Mac-2.local>
2014-03-26 23:25:55 +0800

    Initial Commit

---
+++ MyTradieAngel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyTradieAngel
+//
+//  Created by RenZhe Ahn on 3/26/14.
+//  Copyright (c) 2014 Softaic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyTradieAngel/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyTradieAngel
+//
+//  Created by RenZhe Ahn on 3/26/14.
+//  Copyright (c) 2014 Softaic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
